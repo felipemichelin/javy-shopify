f1bde2f565c16ce011cb2d6ebe955285b1c546e9
felipemichelin <contato@felipemichelin.com>
2021-11-15 17:34:15 -0300

    Update theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -5350,8 +5350,8 @@ matchMedia(theme.config.mediaQuerySmall).addListener(function(mql) {
           // Sale price, if necessary
           if (variant.compare_at_price > variant.price) {
             this.container.querySelector(selectors.comparePrice).innerHTML = theme.Currency.formatMoney(variant.compare_at_price, theme.settings.moneyFormat);
-            var saving_total = 1 - (variant.price) / (variant.compare_at_price);
-            console.log(saving_total).toFixed(2);
+            var saving_total = (1 - (variant.price) / (variant.compare_at_price)).toFixed(2);
+            console.log(saving_total);
             this.container.querySelector(selectors.savingsPct).innerHTML = "felipe";
             this.container.querySelector(selectors.priceWrapper).classList.remove(classes.hidden);
             this.container.querySelector(selectors.price).classList.add(classes.onSale);
