38f04c2882a7d94ab3821d2430cc364c548d3456
felipemichelin <contato@felipemichelin.com>
2021-11-16 16:51:19 -0300

    Update theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -1649,6 +1649,29 @@ matchMedia(theme.config.mediaQuerySmall).addListener(function(mql) {
   
       buildCart: function() {
         theme.cart.getCartProductMarkup().then(this.cartMarkup.bind(this));
+
+           // Custom code for updating freeshipping bar
+           var freeshipping_limit = parseFloat($("#cart_freeshipping_limit").val()) * 100;
+           var short_amount = freeshipping_limit - cart.total_price;
+   
+           var progress_percent = parseInt(cart.total_price * 100 / freeshipping_limit);
+           if(progress_percent > 100) {
+             progress_percent = 100;
+           }
+           $(".drawer__freeshipping-bar .progressbar .progress-wrapper .progress").css("width", progress_percent + "%");
+   
+           if(short_amount <= 0) {
+             $(".drawer__freeshipping-bar .commment-failed").addClass("hide");
+             $(".drawer__freeshipping-bar .commment-success").removeClass("hide");
+           }
+           else {
+             $(".drawer__freeshipping-bar .commment-failed").removeClass("hide");
+             $(".drawer__freeshipping-bar .commment-success").addClass("hide");
+             $(".drawer__freeshipping-bar .short_amount").text("$" + (short_amount / 100).toFixed(2));
+           }
+           ///////////////////////////////////////////////////
+
+           
       },
   
       cartMarkup: function(html) {
@@ -1677,28 +1700,9 @@ matchMedia(theme.config.mediaQuerySmall).addListener(function(mql) {
         this.subtotal.innerHTML = theme.Currency.formatMoney(subtotal, theme.settings.moneyFormat);
   
 
-                // Custom code for updating freeshipping bar
-                var freeshipping_limit = parseFloat($("#cart_freeshipping_limit").val()) * 100;
-                var short_amount = freeshipping_limit - cart.total_price;
-        
-                var progress_percent = parseInt(cart.total_price * 100 / freeshipping_limit);
-                if(progress_percent > 100) {
-                  progress_percent = 100;
-                }
-                $(".drawer__freeshipping-bar .progressbar .progress-wrapper .progress").css("width", progress_percent + "%");
-        
-                if(short_amount <= 0) {
-                  $(".drawer__freeshipping-bar .commment-failed").addClass("hide");
-                  $(".drawer__freeshipping-bar .commment-success").removeClass("hide");
-                }
-                else {
-                  $(".drawer__freeshipping-bar .commment-failed").removeClass("hide");
-                  $(".drawer__freeshipping-bar .commment-success").addClass("hide");
-                  $(".drawer__freeshipping-bar .short_amount").text("$" + (short_amount / 100).toFixed(2));
-                }
-                ///////////////////////////////////////////////////
+             
 
-                
+
         this.reInit();
   
         if (window.AOS) { AOS.refreshHard() }
