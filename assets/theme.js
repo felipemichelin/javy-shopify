9e0f6cf1cea984bdffb2a19c7ee28c7f5d31a909
felipemichelin <contato@felipemichelin.com>
2021-11-16 23:08:14 -0300

    Update theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -1618,7 +1618,7 @@ matchMedia(theme.config.mediaQuerySmall).addListener(function(mql) {
           e.preventDefault();
           var varid = $(this).attr("data-variantid");
           var btn = $(this);
-          var addon = btn.parents(".addon_products");
+          var addon = btn.parents(".cart-addons");
 
           var data = {
             "quantity": 1,
